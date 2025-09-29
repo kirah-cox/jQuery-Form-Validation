889546172eea86e64246ad6bfda282b3380d63b8
Kirah Cox <kirahanncox@gmail.com>
2025-09-29 17:52:07 -0600

    finished assignment

---
+++ form validation/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿
+
+$(document).ready(function() {
+    $('#registration-form').submit(function(event) {
+        event.preventDefault();
+        
+        $('.error').text('');
+        
+        let name = $('#username').val();
+        if (name.trim() === '') {
+            $('#username-error').text('Username is required.');
+        }
+
+        let password = $('#password').val();
+        if (password.length < 8) {
+            $('#password-error').text('Password must be at least 8 characters long.');
+        }
+
+        let confirmPassword = $('#confirmpassword').val();
+        if (confirmPassword != password) {
+            $('#confirmpassword-error').text('Passwords do not match.');
+        }
+
+        if ($('.error').text() === '') {
+            alert('Login successful!');
+        }
+
+        $('#username').val('');
+        $('#password').val('');
+        $('#confirmpassword').val('');
+    });
+});
